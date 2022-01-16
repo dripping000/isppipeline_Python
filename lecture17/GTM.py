bcccd8ca0f28a1d77d8d2cc801cf88794450f923
WaterdropsKun <18761437220@163.com>
2022-01-16 13:59:51 +0800

    Modify [Optimize the code]
    1.Gamma GTM
    
    Signed-off-by: WaterdropsKun <18761437220@163.com>

--- lecture17/GTM.py
+++ lecture17/GTM.py
@@ -1,40 +1,36 @@
+import sys
 import numpy as np
 from matplotlib import pyplot as plt
-import sys
-import color_utils as color
-import cv2 as cv
-import os
 from scipy.interpolate import splev, splrep
+
+import cv2 as cv
+
+import color_utils as color
 
 
 def RGB_luminance(RGB):
-
     Y = 0.299*RGB[:,:,0]+0.5877*RGB[:,:,1]+0.114*RGB[:,:,2]
-
     return Y
+
 
 def log_average(a, epsilon=sys.float_info.epsilon):
     a = a.astype(np.float)
     average = np.exp(np.average(np.log(a + epsilon)))
     return average
 
+
 def tonemapping_operator_simple(RGB):
     RGB = RGB.astype(np.float)
-
     return RGB / (RGB + 1)
 
 
-
 def tonemapping_operator_gamma(RGB, gamma=1, EV=0):
-
-
     RGB = RGB.astype(np.float)
 
     exposure = 2 ** EV
     RGB = (exposure * RGB) ** (1 / gamma)
 
     return RGB
-
 
 
 def tonemapping_operator_logarithmic(RGB,
@@ -55,8 +51,9 @@ def tonemapping_operator_logarithmic(RGB,
     #调整广播
 
     return RGB
-def tonemapping_operator_normalisation(RGB):
 
+
+def tonemapping_operator_normalisation(RGB):
 
     RGB = RGB.astype(np.float)
 
@@ -66,6 +63,7 @@ def tonemapping_operator_normalisation(RGB):
     RGB = RGB / L_max
 
     return RGB
+
 
 def tonemapping_operator_exponential(RGB,
                                      q=1,
@@ -87,7 +85,6 @@ def tonemapping_operator_exponential(RGB,
 
 def tonemapping_operator_logarithmic_mapping(
         RGB, p=1, q=1):
-
 
     RGB = RGB.astype(np.float)
 
@@ -166,7 +163,6 @@ def tonemapping_operator_Reinhard2004(RGB,
                                       c=0
                                      ):
 
-
     RGB = RGB.astype(np.float)
 
     C_av = np.array((np.average(RGB[..., 0]), np.average(RGB[..., 1]),
@@ -233,56 +229,69 @@ def tonemapping_operator_aces(RGB,adapted_lum=1):
     RGB=(RGB * (A * RGB + B)) / (RGB * (C * RGB + D) + E)
     return RGB
 
-##下面都是曲线
 
-
-def gamma_cr(x,gamma):
+# 下面都是曲线
+def gamma_cr(x, gamma):
     y = x ** (gamma)
     return y
 
 
-def logarithmic_cr(x,q=1,k=1):
+def logarithmic_cr(x, q=1, k=1):
     x = x.astype(np.float)
+
     q = 1 if q < 1 else q
     k = 1 if k < 1 else k
+
     L_max = np.max(x)
-    L_d = np.log10(1 + x * q) / np.log10(1 + L_max * k)
+    L_d = np.log10(1+q*x)/np.log10(1+k*L_max)
     y= x*L_d/x
     return y
 
-def sigmod_cr(x,b,c):
-    y=((x**b)/(((c)**b)+(x**b)))
+
+def sigmod_cr(x, b, c):
+    x = x.astype(np.int64)  # 数据类型越界
+    y = ((x**b)/(((c)**b)+(x**b)))
     return y
 
-def log_normalizing(x,q=10,K=50):
-    y=np.log10(1+q*x)/np.log10(1+K*255)
+
+def log_normalizing(x, q=10, K=50):
+    y = np.log10(1+q*x)/np.log10(1+K*255)
     return y
 
-#常见的用法
-def global_tone_mapping(HDRIMG, s = 0.6,gamma = 2.2,max_value=1):
-    HDRIMG=HDRIMG/np.max(HDRIMG)
+
+# 常见的用法
+def global_tone_mapping(HDRIMG, s=0.6, gamma=2.2, max_value=1):
+    HDRIMG = HDRIMG / np.max(HDRIMG)
+
     LDRIMG = np.empty_like(HDRIMG)
+
     X = np.empty((HDRIMG.shape[0], HDRIMG.shape[1]))
     LOG_X = np.empty_like(X)
     LOG_X_0 = np.empty(1)
     LOG_X_hat = np.empty_like(X)
+
     DBL_MIN = sys.float_info.min
     X_0 = np.max(HDRIMG)
+
     # Gamma compression
     X = HDRIMG
 
-    LOG_X_0=np.log2(X_0 + DBL_MIN)
-    LOG_X=np.log2(X + DBL_MIN)
+    LOG_X_0 = np.log2(X_0 + DBL_MIN)
+    LOG_X = np.log2(X + DBL_MIN)
     LOG_X_hat = s * (LOG_X - LOG_X_0) + LOG_X_0
+
     # Restore log(X_hat) to X_hat, and store them to LDRIMG
     np.power(2.0, LOG_X_hat, LDRIMG)
+
     # Gamma Correction
     np.power(LDRIMG, (1.0/gamma), LDRIMG)
+
     # Fix out of range pixels
     LDRIMG[LDRIMG < 0.0] = 0.0
     LDRIMG[LDRIMG > 1.0] = 1.0
     LDRIMG = np.round(LDRIMG*255).astype("uint8")
     return LDRIMG
+
 
 def apply_matrix(input_array, matrix):
     img_out = np.zeros_like(input_array)
@@ -307,38 +316,47 @@ def tone_curve_correction(img_rgb, xs=(0, 64, 128, 192, 256), ys=(0, 64, 128, 19
 
 
 if __name__ == "__main__":
-    # read image
-    # read_image
-    x = np.arange(256)
-    # x=np.array([0.00001,0.0001,0.001,0.01,0.1,1,10,100,1000,10000])
-    #
-
     maxvalue = 1
-    image_BRG = cv.imread('9C4A0034-a460e29cd9.exr',cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
-    image=np.empty_like(image_BRG)
-    image[:,:,0]=image_BRG[:,:,2]
+
+    # read_image
+    image_BRG = cv.imread('9C4A0034-a460e29cd9.exr', cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
+    image = np.empty_like(image_BRG)
+    image[:, :, 0] = image_BRG[:, :, 2]
     image[:, :, 1] = image_BRG[:, :, 1]
     image[:, :, 2] = image_BRG[:, :, 0]
     if (np.max(image) <= 1):
         image = image * maxvalue
-    #new_image=global_tone_mapping(image)
-    #new_image=gamma_cr(image, 1/2.2)
-    new_image=image*255
-    new_image=sigmod_cr(new_image,0.7,8) #2 16
-    new_image = gamma_cr(new_image, 1 / 2.2)
-    #new_image=log_normalizing(new_image,q=15,K=100)
-    new_image2=tone_curve_correction(new_image)
-    #new_image2=tonemapping_operator_filmic(new_image)
-    #y=sigmod_cr(x,0.7, 4)
-    y = sigmod_cr(x, 4, 100)
-    #y=log_normalizing(x,q=15,K=100)
-    #y=logarithmic_cr(x,q=15,k=100)
-    #x = np.log10(x)
-    plt.plot(x, y)
-    plt.show()
-    color.rgb_show(image )
+
+    # new_image = global_tone_mapping(image)
+    # new_image = gamma_cr(image, 1/2.2)
+    new_image = image * 255  # DebugMK
+    new_image = sigmod_cr(new_image, 0.7, 8)  # 2 16
+    new_image = gamma_cr(new_image, 1/2.2)
+    # new_image = log_normalizing(new_image, q=15, K=100)
+
+    new_image2 = tone_curve_correction(new_image)
+    # new_image2 = tonemapping_operator_filmic(new_image)
+
+    color.rgb_show(image)
     color.rgb_show(new_image)
     color.rgb_show(new_image2)
-    # x=np.log10(x)
-    # plt.plot(x,y)
+
+
+    x = np.arange(256)
+    # x = np.array([0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000])
+
+    # y = sigmod_cr(x, 1, 100)
+    # y1 = sigmod_cr(x, 2, 100)
+    # y2 = sigmod_cr(x, 4, 100)
+    # plt.plot(x, y, color='r')
+    # plt.plot(x, y1, color='g')
+    # plt.plot(x, y2, color='b')
+    # plt.show()
+
+    # y = log_normalizing(x, q=15, K=100)
+    # y1 = logarithmic_cr(x, q=15, k=100)
+    # y2 = np.log10(x)
+    # plt.plot(x, y, color='r')
+    # plt.plot(x, y1, color='g')
+    # plt.plot(x, y2, color='b')
     # plt.show()
