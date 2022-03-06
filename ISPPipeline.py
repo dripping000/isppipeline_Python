1029582e250775db4c874e6063967117dbcd54cf
WaterdropsKun <18761437220@163.com>
2022-03-06 09:33:27 +0800

    Modify [Optimize the code] ISPPipeline.py
    Signed-off-by: WaterdropsKun <18761437220@163.com>

--- ISPPipeline.py
+++ ISPPipeline.py
@@ -26,6 +26,16 @@ import ISP_LTM.ltm
 if __name__ == "__main__":
     print("ISPPipeline_start")
 
+
+    BLC_flag = 0
+    DPC_flag = 0
+    BNR_flag = 0
+
+    Demosaic_flag = 1
+    Sharpen_flag = 1
+    LTM_flag = 1
+
+
     # /* read raw */
     file_name = "./Resource/DSC16_1339_768x512_rggb_wait_dpc.raw"
 
@@ -45,46 +55,52 @@ if __name__ == "__main__":
     raw = raw.astype(np.float)
 
 
-    # /* BLC */
-    # clip_range--->clip_range
-    # raw = ISP_BLC.blc.interface(raw, width, height, BayerPatternType, clip_range)
-    # plained_raw.DebugMK_raw("./Resource/test_BLC.bin", "./Resource/test_BLC.bmp", raw, clip_range)
+    if BLC_flag == 1:
+        # /* BLC */
+        # clip_range--->clip_range
+        raw = ISP_BLC.blc.interface(raw, width, height, BayerPatternType, clip_range)
+        plained_raw.DebugMK_raw("./Resource/test_BLC.bin", "./Resource/test_BLC.bmp", raw, clip_range)
 
 
-    # /* DPC */
-    # clip_range--->clip_range
-    raw = ISP_DPC.dpc.interface(raw, width, height, BayerPatternType, clip_range)
-    plained_raw.DebugMK_raw("./Resource/test_DPC.bin", "./Resource/test_DPC.bmp", raw, clip_range)
+    if DPC_flag == 1:
+        # /* DPC */
+        # clip_range--->clip_range
+        raw = ISP_DPC.dpc.interface(raw, width, height, BayerPatternType, clip_range)
+        plained_raw.DebugMK_raw("./Resource/test_DPC.bin", "./Resource/test_DPC.bmp", raw, clip_range)
 
 
-    # /* BNR */
-    # clip_range--->clip_range
-    # raw = ISP_NR.hvs_denoise.interface(raw, width, height, BayerPatternType, clip_range)
-    # plained_raw.DebugMK_raw("./Resource/test_BNR.bin", "./Resource/test_BNR.bmp", raw, clip_range)
+    if BNR_flag == 1:
+        # /* BNR */
+        # clip_range--->clip_range
+        raw = ISP_NR.hvs_denoise.interface(raw, width, height, BayerPatternType, clip_range)
+        plained_raw.DebugMK_raw("./Resource/test_BNR.bin", "./Resource/test_BNR.bmp", raw, clip_range)
 
 
-    # /* Demosaic */
-    # clip_range--->clip_range gamma_off
-    image = ISP_Demosaic.demosaic.interface(raw, width, height, BayerPatternType, clip_range)
-    print("/* Demosaic */ shape", image.shape)
-    print("/* Demosaic */ max", np.max(image), "min", np.min(image), "\n")
-    plained_raw.DebugMK_raw("./Resource/test_Demosaic.bin", "./Resource/test_Demosaic.bmp", image, clip_range)
+    if Demosaic_flag == 1:
+        # /* Demosaic */
+        # clip_range--->clip_range gamma_off
+        image = ISP_Demosaic.demosaic.interface(raw, width, height, BayerPatternType, clip_range)
+        print("/* Demosaic */ shape", image.shape)
+        print("/* Demosaic */ max", np.max(image), "min", np.min(image), "\n")
+        plained_raw.DebugMK_raw("./Resource/test_Demosaic.bin", "./Resource/test_Demosaic.bmp", image, clip_range)
 
 
-    # /* Sharpen */
-    # clip_range--->clip_range
-    image = ISP_Sharpen.sharpen.interface(image, width, height, clip_range)
-    print("/* Sharpen */ shape", image.shape)
-    print("/* Sharpen */ max", np.max(image), "min", np.min(image), "\n")
-    plained_raw.DebugMK_raw("./Resource/test_Sharpen.bin", "./Resource/test_Sharpen.bmp", image, clip_range)
+    if (Demosaic_flag == 1) and (Sharpen_flag == 1):
+        # /* Sharpen */
+        # clip_range--->clip_range
+        image = ISP_Sharpen.sharpen.interface(image, width, height, clip_range)
+        print("/* Sharpen */ shape", image.shape)
+        print("/* Sharpen */ max", np.max(image), "min", np.min(image), "\n")
+        plained_raw.DebugMK_raw("./Resource/test_Sharpen.bin", "./Resource/test_Sharpen.bmp", image, clip_range)
 
 
-    # /* LTM */
-    # clip_range--->clip_range gamma_on
-    image = ISP_LTM.ltm.interface(image, width, height, clip_range)
-    print("/* LTM */ shape", image.shape)
-    print("/* LTM */ max", np.max(image), "min", np.min(image), "\n")
-    plained_raw.DebugMK_raw("./Resource/test_LTM.bin", "./Resource/test_LTM.bmp", image, clip_range)
+    if (Demosaic_flag == 1) and (LTM_flag == 1):
+        # /* LTM */
+        # clip_range--->clip_range gamma_on
+        image = ISP_LTM.ltm.interface(image, width, height, clip_range)
+        print("/* LTM */ shape", image.shape)
+        print("/* LTM */ max", np.max(image), "min", np.min(image), "\n")
+        plained_raw.DebugMK_raw("./Resource/test_LTM.bin", "./Resource/test_LTM.bmp", image, clip_range)
 
 
     print("ISPPipeline_end")
