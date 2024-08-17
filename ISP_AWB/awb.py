95a49a1e3a516f6ea3f0ea13acf77196ef025a22
WaterdropsKun <18761437220@163.com>
2024-08-17 08:22:53 +0800

    Add ISP_AWB
    1.show_awb_stats.py
    
    Signed-off-by: WaterdropsKun <18761437220@163.com>

--- ISP_AWB/awb.py
+++ ISP_AWB/awb.py
@@ -5,6 +5,24 @@ import plained_raw
 import raw_image
 import raw_image_show
 
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+import os
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__)) + "\\"
+rootpath = str(curPath)+"..\\"
+syspath = sys.path
+depth = rootpath.count("\\") - 1
+sys.path = []
+sys.path.append(rootpath)  # 将工程根目录加入到python搜索路径中
+# sys.path.extend([rootpath+i for i in os.listdir(rootpath) if i[depth]!="."])  # 将工程目录下的一级目录添加到python搜索路径中
+sys.path.extend(syspath)
+# print(sys.path)
+import ISP_BLC.blc
+from isp_utils import plained_raw
+
 
 def create_awb_stats_data(img, stats_size_width, stats_size_height, pattern):
     # 分开四个颜色通道
@@ -31,11 +49,33 @@ def create_awb_stats_data(img, stats_size_width, stats_size_height, pattern):
             G_ = (GR_+GB_)/2
             B_ = B[y:y+block_size_height, x:x+block_size_width].mean()
 
-            R_awb_stats[block_y_num, block_x_num] = R_ / G_
-            G_awb_stats[block_y_num, block_x_num] = G_ / G_
-            B_awb_stats[block_y_num, block_x_num] = B_ / G_
+            # R_awb_stats[block_y_num, block_x_num] = R_ / G_
+            # G_awb_stats[block_y_num, block_x_num] = G_ / G_
+            # B_awb_stats[block_y_num, block_x_num] = B_ / G_
+
+            R_awb_stats[block_y_num, block_x_num] = R_
+            G_awb_stats[block_y_num, block_x_num] = G_
+            B_awb_stats[block_y_num, block_x_num] = B_
+
 
     # raw_image_show.raw_image_show_3D(R_awb_stats,stats_size_height,stats_size_width)  # DebugMK
+    # raw_image_show.raw_image_show_3D(G_awb_stats,stats_size_height,stats_size_width)  # DebugMK
+    # raw_image_show.raw_image_show_3D(B_awb_stats,stats_size_height,stats_size_width)  # DebugMK
+
+    fig =  plt.figure()
+    ax = Axes3D(fig)
+    ax = plt.subplot(1,1,1,projection='3d')
+    X = np.arange(0, stats_size_width)
+    Y = np.arange(0, stats_size_height)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X ** 2 + Y ** 2)
+    # ax.plot_surface(X, Y, R_awb_stats, rstride=1, cstride=1, cmap='rainbow')
+    ax.plot_wireframe(X, Y, R_awb_stats, rstride=10, cstride=10, color='red')
+    ax.plot_wireframe(X, Y, G_awb_stats, rstride=10, cstride=10, color='green')
+    ax.plot_wireframe(X, Y, B_awb_stats, rstride=10, cstride=10, color='blue')
+    plt.show()
+    print('show')
+
 
     return R_awb_stats, G_awb_stats, B_awb_stats
 
@@ -46,21 +86,23 @@ def interface(raw, width, height, BayerPatternType, clip_range):
 
 
 if __name__ == "__main__":
-    width = 8192
-    height = 6144
+    width = 4096
+    height = 3072
 
     pattern = "BGGR"
-    clip_range = [0, 2**10-1]
+    clip_range = [0, 2**14-1]
 
     stats_size_width = 64
     stats_size_height = 48
 
     # 读取显示RAW文件
-    # img = plained_raw.read_plained_file("./Resource/p[ZSLSnapshotYUVHAL]_req[1]_batch[0]_BPS[0]_[in]_port[0]_w[8192]_h[6144]_stride[16384]_scanline[6144]_20231027_144501_733424.raw", height, width, shift_bits=0)
+    raw = plained_raw.read_plained_file("./Resource/D65.raw", height, width, shift_bits=0)
     # raw_image_show.raw_image_show_fullsize(img/clip_range[1], height, width)
 
+    raw = ISP_BLC.blc.interface(raw, width, height, pattern, clip_range)
+    plained_raw.DebugMK_raw("./Resource/test_BLC.bin", "./Resource/test_BLC.bmp", raw, clip_range)
+
     # 生成awb stats
-    raw = plained_raw.read_plained_file("./Resource/p[ZSLSnapshotYUVHAL]_req[1]_batch[0]_BPS[0]_[in]_port[0]_w[8192]_h[6144]_stride[16384]_scanline[6144]_20231027_144501_733424.raw", height, width, shift_bits=0)
     R_awb_stats, G_awb_stats, B_awb_stats = create_awb_stats_data(raw, stats_size_width, stats_size_height, pattern)
 
 
@@ -74,6 +116,7 @@ if __name__ == "__main__":
     print(data)
 
     data['wb_gain']['R_awb_stats'] = R_awb_stats.flatten().tolist()
+    data['wb_gain']['G_awb_stats'] = G_awb_stats.flatten().tolist()
     data['wb_gain']['B_awb_stats'] = B_awb_stats.flatten().tolist()
 
     # 保存修改后的JSON文件
---
+++ ISP_AWB/show_awb_stats.py
@@ -0,0 +1,254 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import traceback
+
+import json
+
+
+def format_bit(x, num=6):
+    return round(x, num)
+
+
+fig, ax = plt.subplots()
+po_annotation1 = []
+def plot_annotations(plt, point_x, point_y, text, po_annotation):
+    point, = plt.plot(point_x, point_y, 'o', c='darkgreen', markersize=2)
+    # 标注偏移量
+    offset1 = 30
+    offset2 = 30
+    # 标注框
+    bbox1 = dict(boxstyle="round", fc="lightgreen", alpha=0.6)
+    # 标注箭头
+    arrowprops1 = dict(arrowstyle="->", connectionstyle="arc3,rad=0.")
+    # 标注
+    annotation = plt.annotate(text, xy=(point_x, point_y), xytext=(offset1, offset2), textcoords="offset points",
+                            bbox=bbox1, arrowprops=arrowprops1, size=15)
+    # 默认鼠标未指向时不显示标注信息
+    annotation.set_visible(False)
+    po_annotation.append([point, annotation])
+
+
+# 定义鼠标响应函数
+def on_move(event):
+    visibility_changed = False
+    for point, annotation in po_annotation1:
+        should_be_visible = (point.contains(event)[0] == True)
+
+        if should_be_visible != annotation.get_visible():
+            visibility_changed = True
+            annotation.set_visible(should_be_visible)
+
+    if visibility_changed:
+        plt.draw()
+
+
+def plot_list_wbgain(plt, list, quantization=1.0, color='y'):
+    # print("\n")
+    for i in range(len(list)):
+        print(i, list[i])
+        print(i, 1/(list[i][0]/quantization), 1/(list[i][1]/quantization))
+
+        if i <= (len(list) - 2):
+            plt.plot([1/(list[i][0]/quantization), 1/(list[i+1][0]/quantization)], [1/(list[i][1]/quantization), 1/(list[i+1][1]/quantization)], color=color, linestyle='-')
+        if i == (len(list) - 1):
+            plt.plot([1/(list[i][0]/quantization), 1/(list[0][0]/quantization)], [1/(list[i][1]/quantization), 1/(list[0][1]/quantization)], color=color, linestyle='-')
+
+
+def plot_list_1_wbgain(plt, list, quantization=1.0, color='y'):
+    # print("\n")
+    for i in range(len(list)):
+        print(i, list[i])
+        print(i, (list[i][0]/quantization), (list[i][1]/quantization))
+
+        if i <= (len(list) - 2):
+            plt.plot([(list[i][0]/quantization), (list[i+1][0]/quantization)], [(list[i][1]/quantization), (list[i+1][1]/quantization)], color=color, linestyle='-')
+        if i == (len(list) - 1):
+            plt.plot([(list[i][0]/quantization), (list[0][0]/quantization)], [(list[i][1]/quantization), (list[0][1]/quantization)], color=color, linestyle='-')
+
+
+# 判断是否与水平射线相交
+def is_intersect(p1, p2, point):
+    if min(p1[1], p2[1]) <= point[1] <= max(p1[1], p2[1]):
+        x = p1[0] + (point[1] - p1[1]) / (p2[1] - p1[1]) * (p2[0] - p1[0])
+        if point[0] < x:
+            return True
+    return False
+
+def point_in_polygon(point, polygon):
+    # 统计相交点的个数
+    count = 0
+    # 遍历多边形的所有边
+    for i in range(len(polygon)):
+        # 边的两个顶点
+        p1 = polygon[i]
+        p2 = polygon[(i + 1) % len(polygon)]
+        # 判断边是否与水平射线相交
+        if is_intersect(p1, p2, point):
+            count += 1
+    # 判断点的位置
+    if count % 2 == 1:
+        return True  # 在多边形内部
+    else:
+        return False  # 在多边形外部
+
+
+def trigger_weight(x, trigger_x, trigger_y):
+    for i in range(len(trigger_x)):
+        if x >= trigger_x[i] and x <= trigger_x[i+1]:
+            weight = (trigger_y[i+1] - trigger_y[i]) / (trigger_x[i+1] - trigger_x[i]) * (x - trigger_x[i]) + trigger_y[i]
+            break
+
+    return weight
+
+
+if __name__ == '__main__':
+    with open('./Resource/isp_config_cannon_.json', 'r') as f:
+        data = json.load(f)
+
+    # 打印JSON数据
+    # print(data)
+
+    r_stat = data['wb_gain']['R_awb_stats']
+    g_stat = data['wb_gain']['G_awb_stats']
+    b_stat = data['wb_gain']['B_awb_stats']
+
+
+    plt.style.use('ggplot')
+
+    stat_index = 0  # Debug 多帧数据时，用于选择第几帧数据
+    stat_num = 1  # Debug stat_index开始，连续显示stat_num帧数据
+
+    Dark_Stats_Threshold = 0  # Debug
+    Sat_Stats_Threshold = 65535  # Debug
+
+
+    SA_1_weight_global = 1.0
+
+    polygon_SA1 = [(0.33, 0.80), (0.42, 0.80), (0.42, 0.67), (0.33, 0.67)]
+
+    SA_1_Level1_2ndTri_x = [0,0.04,     0.1,0.3,    0.4,0.6,    0.7,1]
+    SA_1_Level1_2ndTri_y = [0,0,        0.8,0.8,    1,1,        1,1]
+
+
+    for i in range(stat_num):
+        po_annotation1 = []
+
+        plt.clf()
+        SA_1_rgain_sum = 0
+        SA_1_bgain_sum = 0
+        SA_1_num = 0
+        SA_1_rgain = 0
+        SA_1_bgain = 0
+
+        golden_num = 0
+
+        wb_rgain = 0
+        wb_bgain = 0
+
+
+        stat_index_ = stat_index + i
+
+        rgain = []
+        bgain = []
+
+        Dark_Stats_Count = 0
+        Sat_Stats_Count = 0
+        g_min = 65535
+        g_max = 0
+
+        for r in r_stat:
+            # print(r_stat.index(r))
+
+            r_ = float(r_stat[r_stat.index(r)])
+            g_ = float(g_stat[r_stat.index(r)])
+            b_ = float(b_stat[r_stat.index(r)])
+            # print(r, g_, b_)
+
+            if g_ == 0.0:
+                print(r_stat.index(r), r_, g_, b_)
+                g_ = 1
+
+            rgain_min = 0
+            rgain_max = 1.5
+            bgain_min = 0
+            bgain_max = 2
+            rgain_ = np.clip(r_/g_, rgain_min, rgain_max)
+            bgain_ = np.clip(b_/g_, bgain_min, bgain_max)
+
+            V = math.floor(r_stat.index(r) / 64)
+            H = math.floor(r_stat.index(r) - V*64)
+
+            if g_ < g_min:
+                g_min = g_
+            if g_ > g_max:
+                if g_ > 65535:
+                    print(g_, V, H)
+                    pass
+                else:
+                    g_max = g_
+
+            if (r_stat.index(r)%2 == 0):
+                if r_ < Dark_Stats_Threshold or g_ < Dark_Stats_Threshold or b_ < Dark_Stats_Threshold:
+                    Dark_Stats_Count += 1
+                    print('Dark_Stats_Count', g_, V, H)
+
+                elif r_ > Sat_Stats_Threshold or g_ > Sat_Stats_Threshold or b_ > Sat_Stats_Threshold:
+                    Sat_Stats_Count += 1
+                    print('Sat_Stats_Count', g_, V, H)
+                
+                else:
+                    plot_annotations(plt, rgain_, bgain_, str(V)+","+str(H)+": "+str(format_bit(rgain_))+","+str(format_bit(bgain_))+","+str(format_bit(r_))+","+str(format_bit(g_))+","+str(format_bit(b_)), po_annotation1)
+
+                    rgain.append(rgain_)
+                    bgain.append(bgain_)
+
+                    point = (rgain_, bgain_)
+
+                    if (point_in_polygon(point, polygon_SA1) == True):
+                        SA_1_rgain_sum += rgain_
+                        SA_1_bgain_sum += bgain_
+                        SA_1_num += 1
+
+        plt.scatter(rgain, bgain, c='m', s=2)
+        plt.title(str(stat_index_)+": ", fontdict={'fontsize': 10})
+
+
+        if SA_1_num == 0.0:
+            SA_1_num = 1.0
+        
+        SA_1_rgain = SA_1_rgain_sum / SA_1_num
+        SA_1_bgain = SA_1_bgain_sum / SA_1_num
+
+        SA_num_sum = 1536
+
+        SA_1_Level1 = SA_1_num / SA_num_sum
+        SA_1_Level1_CONFIDENCE = trigger_weight(SA_1_Level1, SA_1_Level1_2ndTri_x, SA_1_Level1_2ndTri_y)
+
+        SA_1_weight = SA_1_Level1_CONFIDENCE
+        SA_1_weight = 1.0
+
+        wb_rgain = SA_1_rgain * SA_1_weight
+        wb_bgain = SA_1_bgain * SA_1_weight
+
+        plt.plot(SA_1_rgain, SA_1_bgain, 'o', C='y', markersize=5)
+
+
+    r_g = [0.253272, 0.407010, 0.442099, 0.527150, 0.515729, 0.586529, 0.781099, 1.021902, 1.401907]
+    b_g = [0.939229, 0.773839, 0.728039, 0.578373, 0.447339, 0.448376, 0.363937, 0.297893, 0.187933]
+
+    plt.scatter(r_g, b_g, c='c', s=10, label='AWB')
+    plt.plot(r_g, b_g, color='c', linestyle='-')
+
+    polygon_SA = [(0.17, 1.36), (0.22, 1.36), (0.22, 0.14), (0.17, 0.14)]
+    plot_list_1_wbgain(plt, polygon_SA, quantization=1.0, color='c')
+
+
+    # 鼠标移动事件
+    on_move_id = fig.canvas.mpl_connect('motion_notify_event', on_move)
+
+    # 显示图形
+    plt.show()
+
+    pass
